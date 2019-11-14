7e9d432226537ca439e82a3547794e5979b26620
Robert Tanase <robert@crowdconnected.com>
2019-11-14 10:29:49 +0000

    Cleaned project

--- Indoor Tracking Colocator/ViewController.swift
+++ Indoor Tracking Colocator/ViewController.swift
@@ -11,15 +11,17 @@ import MapwizeUI
 import CCLocation
 
 class ViewController: UIViewController {
-    let venueID = "5daf325c4ddf80001615f1e3" // ETL 2019
-    let accessKey = "HbsGVi689ybLvPr5"
+    
+    let kMapwizeAPIKey = "YOUR_MAPWIZE_API_KEY"
+    let kVenueID = "YOUR_VENUE_ID"
+    let kFloorNumber = 1 // Please contact CrowdConnected if you're not sure about your floor number or have multiple floors
 
     var locationManager: CLLocationManager!
-    var manualLocationProvider: ILIndoorLocationProvider!
+    
     var map: MWZMapwizeView!
     var mapwizeApi: MWZMapwizeApi?
-
     let indoorLocationProvider = ILIndoorLocationProvider()
+    
     var lastIndoorLocation: ILIndoorLocation? {
         didSet {
             if lastIndoorLocation != nil {
@@ -27,52 +29,48 @@ class ViewController: UIViewController {
             }
         }
     }
-    var lastDirection: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        locationManager = CLLocationManager()
-        locationManager.requestAlwaysAuthorization()
-        locationManager.startUpdatingLocation()
-        locationManager.startUpdatingHeading()
         
         configureMap()
-        
         configureCCLocation()
-        
         configureLocationProvider()
     }
 
     private func configureMap() {
-        let mapwizeConfiguration = MWZMapwizeConfiguration(apiKey: "7b63e66f6a5bebaea25fc5a0e276d5ec")
+        let mapwizeConfiguration = MWZMapwizeConfiguration(apiKey: kMapwizeAPIKey)
         mapwizeApi = MWZMapwizeApiFactory.getApi(mapwizeConfiguration:mapwizeConfiguration)
         
-        let fr = view.frame
-        let op = MWZUIOptions()
-        op.centerOnVenueId = venueID
+        let mapFrame = view.frame
+        let mapUIOptions = MWZUIOptions()
+        mapUIOptions.centerOnVenueId = kVenueID
         
-        let ui = MWZMapwizeViewUISettings()
-        ui.compassIsHidden = false
-        ui.floorControllerIsHidden = false
-        ui.menuButtonIsHidden = true
-        ui.followUserButtonIsHidden = false
+        let mapUISettings = MWZMapwizeViewUISettings()
+        mapUISettings.compassIsHidden = false
+        mapUISettings.floorControllerIsHidden = false
+        mapUISettings.menuButtonIsHidden = true
+        mapUISettings.followUserButtonIsHidden = false
         
-        map = MWZMapwizeView(frame: fr, mapwizeConfiguration: mapwizeConfiguration, mapwizeOptions: op, uiSettings: ui)
+        map = MWZMapwizeView(frame: mapFrame,
+                             mapwizeConfiguration: mapwizeConfiguration,
+                             mapwizeOptions: mapUIOptions,
+                             uiSettings: mapUISettings)
         map.delegate = self
         map.followUserButton.delegate = self
+        
+        // Disable followUser button until location is received from IndoorLocationProvider
         map.followUserButton.isEnabled = false
+        
         view.addSubview(map)
     }
     
     private func configureCCLocation() {
+        locationManager = CLLocationManager()
+        locationManager.requestAlwaysAuthorization()
+        
         let ccLocation = CCLocation.sharedInstance
         ccLocation.delegate = self
-        ccLocation.setLoggerLevels(verbose: false,
-                                          info: true,
-                                          debug: false,
-                                          warning: true,
-                                          error: true,
-                                          severe: true)
         ccLocation.registerLocationListener()
     }
     
@@ -82,78 +80,18 @@ class ViewController: UIViewController {
     }
 }
 
-extension ViewController: ILIndoorLocationProviderDelegate {
-    func provider(_ provider: ILIndoorLocationProvider!, didUpdate location: ILIndoorLocation!) {
-        print("Location update")
-    }
-    
-    func provider(_ provider: ILIndoorLocationProvider!, didFailWithError error: Error!) {
-        print("location fail woth error")
-    }
-    
-    func providerDidStart(_ provider: ILIndoorLocationProvider!) {
-        print("provider started")
-    }
-    
-    func providerDidStop(_ provider: ILIndoorLocationProvider!) {
-        print("provider stopped")
-    }
-}
-
-extension ViewController: MWZComponentFollowUserButtonDelegate {
-    func didTapWithoutLocation() {
-        print("Tapped without location")
-    }
-    
-    func followUserButton(_ followUserButton: MWZComponentFollowUserButton!, didChange followUserMode: MWZFollowUserMode) {
-        var mode = ""
-        switch followUserMode {
-        case .none:
-            mode = "None"
-        case .followUser:
-            mode = "Follow User"
-        case .followUserAndHeading:
-            mode = "Follow User & Heading"
-        @unknown default:
-            mode = "Unknown"
-        }
-        print("Changed follow mode to \(mode)")
-        map.mapView.setFollowUserMode(followUserMode)
-    }
-    
-    func followUserButtonRequiresUserLocation(_ followUserButton: MWZComponentFollowUserButton!) -> ILIndoorLocation! {
-        print("Required user location from provider. Location returned (\(lastIndoorLocation!.latitude) ; \(lastIndoorLocation!.longitude))")
-        return lastIndoorLocation
-    }
-    
-    func followUserButtonRequiresFollowUserMode(_ followUserButton: MWZComponentFollowUserButton!) -> MWZFollowUserMode {
-        print("Asked for follow user mode. Mode returned Follow User & Heading")
-        return .followUserAndHeading // this is weird. If you want to track heading, use .followUser only
-    }
-}
-
 extension ViewController: MWZMapwizeViewDelegate {
     func mapwizeViewDidLoad(_ mapwizeView: MWZMapwizeView!) {
-        print("MapWize did load")
         map.mapView.setIndoorLocationProvider(indoorLocationProvider!)
     }
     
-    func mapwizeView(_ mapwizeView: MWZMapwizeView!, didTapOnPlaceInformationButton place: MWZPlace!) {
-        print("MapWize did tap on place \(place?.alias ?? "unknown")")
-    }
+    func mapwizeView(_ mapwizeView: MWZMapwizeView!, didTapOnPlaceInformationButton place: MWZPlace!) { }
     
-    func mapwizeView(_ mapwizeView: MWZMapwizeView!, didTapOnPlaceListInformationButton placeList: MWZPlacelist!) {
-        print("MapWize did tap on place list")
-        print(placeList ?? "list")
-    }
+    func mapwizeView(_ mapwizeView: MWZMapwizeView!, didTapOnPlaceListInformationButton placeList: MWZPlacelist!) { }
 
-    func mapwizeViewDidTap(onFollowWithoutLocation mapwizeView: MWZMapwizeView!) {
-        print("MapWize following without location")
-    }
+    func mapwizeViewDidTap(onFollowWithoutLocation mapwizeView: MWZMapwizeView!) { }
 
-    func mapwizeViewDidTap(onMenu mapwizeView: MWZMapwizeView!) {
-        print("MapWize menu")
-    }
+    func mapwizeViewDidTap(onMenu mapwizeView: MWZMapwizeView!) { }
 
     func mapwizeView(_ mapwizeView: MWZMapwizeView!, shouldShowInformationButtonFor mapwizeObject: MWZObject!) -> Bool {
         if (mapwizeObject is MWZPlace) {
@@ -170,24 +108,25 @@ extension ViewController: MWZMapwizeViewDelegate {
     }
 }
 
-//extension ViewController: CLLocationManagerDelegate {
-//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-//        guard let last = locations.last else {
-//            return
-//        }
-//        lastIndoorLocation = ILIndoorLocation(provider: indoorLocationProvider,
-//                                              latitude: last.coordinate.latitude,
-//                                              longitude: last.coordinate.longitude,
-//                                              floor: 1)
-//
-//        indoorLocationProvider?.dispatchDidUpdate(lastIndoorLocation)
-//    }
-//}
+extension ViewController: MWZComponentFollowUserButtonDelegate {
+    func didTapWithoutLocation() { }
+    
+    func followUserButton(_ followUserButton: MWZComponentFollowUserButton!, didChange followUserMode: MWZFollowUserMode) {
+        map.mapView.setFollowUserMode(followUserMode)
+    }
+    
+    func followUserButtonRequiresUserLocation(_ followUserButton: MWZComponentFollowUserButton!) -> ILIndoorLocation! {
+        return lastIndoorLocation
+    }
+    
+    func followUserButtonRequiresFollowUserMode(_ followUserButton: MWZComponentFollowUserButton!) -> MWZFollowUserMode {
+        return .followUserAndHeading
+    }
+}
 
 extension ViewController: CCLocationDelegate {
     func ccLocationDidConnect() {
         // CCLocation library connected successfully
-        print("CCLocation connected successfully")
     }
     
     func ccLocationDidFailWithError(error: Error) {
@@ -195,7 +134,6 @@ extension ViewController: CCLocationDelegate {
     }
     
     func didReceiveCCLocation(_ location: LocationResponse) {
-        print("CCLocation returned location from server")
         lastIndoorLocation = ILIndoorLocation(provider: indoorLocationProvider,
                                               latitude: location.latitude,
                                               longitude: location.longitude,
@@ -205,5 +143,23 @@ extension ViewController: CCLocationDelegate {
     
     func didFailToUpdateCCLocation() {
         // Failed to update location provider state for CCLocation
+    }
+}
+
+extension ViewController: ILIndoorLocationProviderDelegate {
+    func provider(_ provider: ILIndoorLocationProvider!, didUpdate location: ILIndoorLocation!) {
+        // Location did update
+    }
+    
+    func provider(_ provider: ILIndoorLocationProvider!, didFailWithError error: Error!) {
+        // Location failed with error
+    }
+    
+    func providerDidStart(_ provider: ILIndoorLocationProvider!) {
+        // Provider did start
+    }
+    
+    func providerDidStop(_ provider: ILIndoorLocationProvider!) {
+        // Provider did stop
     }
 }
--- Podfile
+++ Podfile
@@ -1,12 +1,7 @@
-# Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
-
 target 'Indoor Tracking Colocator' do
-  # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
-pod 'MapwizeUI', '~> 2.0'
-pod 'CCLocation'
-  # Pods for Indoor Tracking Colocator
+  pod 'MapwizeUI', '~> 2.0'
+  pod 'CCLocation'
 
 end
